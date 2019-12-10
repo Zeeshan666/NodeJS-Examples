5f05e0935787db1c130ae1644a1dde26dfea7cca
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2019-12-10 12:42:27 -0800

    simple example

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var rect = {
+	perimeter: (x, y) => (2*(x+y)),
+	area: (x, y) => (x*y)
+};
+
+
+const rectanlge =(l,b)=>{
+console.log("Solving for rectangle with l = " + l + " and b = " + b);
+if (l <= 0 || b <= 0) {
+    console.log("Rectangle dimensions should be greater than zero:  l = "
+           + l + ",  and b = " + b);
+}else{
+    console.log("the area of rectangle is" +rect.area(l,b))
+    console.log("the perimeter of rectangle is" +rect.perimeter(l,b))
+    
+
+}
+}
+
+rectanlge(2,4);
+rectanlge(3,5);
+rectanlge(0,5);
+rectanlge(-3,5);
