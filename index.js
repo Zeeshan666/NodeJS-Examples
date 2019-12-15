83d8fc888d7d1b927324a20a82d27b07d08e27ad
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2019-12-14 23:57:11 -0800

    call backs and error

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+var rect = require("./rectangle");
 
-// var rect = {
-// 	perimeter: (x, y) => (2*(x+y)),
-// 	area: (x, y) => (x*y)
-// };
-
-var rect = require('./rectangle')
-
-const rectanlge =(l,b)=>{
-console.log("Solving for rectangle with l = " + l + " and b = " + b);
-if (l <= 0 || b <= 0) {
-    console.log("Rectangle dimensions should be greater than zero:  l = "
-           + l + ",  and b = " + b);
-}else{
-    console.log("the area of rectangle is" +rect.area(l,b))
-    console.log("the perimeter of rectangle is" +rect.peremeter(l,b))
-    
-
+function solveRect(l, b) {
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR: ", err.message);
+    } else {
+      console.log(
+        "The area of the rectangle of dimensions l = " +
+          l +
+          " and b = " +
+          b +
+          " is " +
+          rectangle.area()
+      );
+      console.log(
+        "The perimeter of the rectangle of dimensions l = " +
+          l +
+          " and b = " +
+          b +
+          " is " +
+          rectangle.perimeter()
+      );
+    }
+  });
+  console.log("This statement after the call to rect()");
 }
-}
 
-rectanlge(2,4);
-rectanlge(3,5);
-rectanlge(0,5);
-rectanlge(-3,5);
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
